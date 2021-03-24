83e10b5acde1e73ed867d993e7ae22bc25b12453
josemamunoz <josema.munoz@gmail.com>
2021-03-23 22:02:41 -0300

    readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import background from "./img/background.png";
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="CountryQuizz" style={{ backgroundImage: `url(${background})`}}>
+      <div className="container">
+      <div className="quiz">
+        Cuadrado del quiz
+        <div className="title">Country Quiz</div>
+        <div className="contenido">
+          
+          <div className="question">
+            Kuala Lumpur us the capital of
+          </div>
+          <div className="respuesta-A">
+            A vietnam
+          </div>
+          <div className="respuesta-B">
+            B Malasya
+          </div>
+          <div className="respuesta-C">
+            C Sweden
+          </div>
+          <div className="respuesta-D">
+            D Austria
+          </div>
+        </div>
+      </div>
+      </div>
+      <div className="credits">
+        created by <a target="_blank" rel="noreferrer" href="https://devchallenges.io/portfolio/josemamunoz" className="linksexternos">josemamunoz</a> - <a target="_blank" rel="noreferrer" href="https://devchallenges.io/" className="linksexternos">devchallenges.io</a>
+      </div>
     </div>
   );
 }
